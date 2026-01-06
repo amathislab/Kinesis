7073cb9ba6fceb452eb994fcc6cef3420273401b
merc-s <merkourios.simos@epfl.ch>
2026-01-06 09:49:02 +0100

    Update configuration files for fullbody and legs models, including data directory adjustments and config name changes in initial_pose script.

--- src/utils/initial_pose.py
+++ src/utils/initial_pose.py
@@ -16,7 +16,7 @@ import numpy as np
 @hydra.main(
     version_base=None,
     config_path="../data/cfg",
-    config_name="config",
+    config_name="config_legs.yaml",
 )
 def main(cfg):
     initial_pose_dict = {}
@@ -48,7 +48,11 @@ def main(cfg):
             new_data[motion_key][new_key] = initial_pose_dict[motion_key][frame_key]
             print(f'Old key: {frame_key}, New key: {new_key}')
 
-    joblib.dump(new_data, f'data/initial_pose/initial_pose_{cfg.exp_name}.pkl')
+    if cfg.run.motion_file == "data/kit_train_motion_dict.pkl":
+        dataset_type = "train"
+    else:
+        dataset_type = "test"
+    joblib.dump(new_data, f'data/initial_pose/{cfg.datadir}/initial_pose_{dataset_type}.pkl')
 
 if __name__ == "__main__":
-    main()
+    main()  
