1198a054772d90c02a7a70d2008c3b29f9e97d47
merc-s <merkourios.simos@epfl.ch>
2026-01-06 09:35:50 +0100

    Switch to different primary config files per embodiment and rewrite kit-locomotion script to reflect changes and accept model argument

--- scripts/kit-locomotion.sh
+++ scripts/kit-locomotion.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 # default values
-mode=test
+model=legs
+dataset=test
 headless=False
 
 # parse arguments
 while [[ $# -gt 0 ]]; do
     case $1 in
-        --mode)
-            mode=$2
+        --model)
+            model=$2
+            shift
+            shift
+            ;;
+        --dataset)
+            dataset=$2
             shift
             shift
             ;;
@@ -24,21 +30,33 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-if [[ $mode == "train" ]]; then
-    motion_file="data/kit_train_motion_dict.pkl"
-    initial_pose_file="data/initial_pose/initial_pose_train.pkl"
-elif [[ $mode == "test" ]]; then
-    motion_file="data/kit_test_motion_dict.pkl"
-    initial_pose_file="data/initial_pose/initial_pose_test.pkl"
+if [[ $model == "legs" ]]; then
+    echo "Using legs model"
+    config_name="config_legs.yaml"
+    run_config="eval_run_legs"
+    initial_pose_dir="data/initial_pose/legs_model"
 else
-    echo "Invalid mode: $mode. Use 'train' or 'test'."
+    echo "Invalid model: $model. Currently only 'legs' model is supported."
+    exit 1
+fi
+
+if [[ $dataset == "train" ]]; then
+    motion_file="data/kit_train_motion_dict.pkl"
+    initial_pose_file="${initial_pose_dir}/initial_pose_train.pkl"
+elif [[ $dataset == "test" ]]; then
+    motion_file="data/kit_test_motion_dict.pkl"
+    initial_pose_file="${initial_pose_dir}/initial_pose_test.pkl"
+else
+    echo "Invalid dataset: $dataset. Use 'train' or 'test'."
     exit 1
 fi
 
 # Run the script
-python src/run.py exp_name=kinesis-moe-imitation \
+python src/run.py \
+    --config-name ${config_name} \
+    exp_name=kinesis-moe-imitation \
     epoch=-1 \
-    run=eval_run \
+    run=${run_config} \
     run.headless=${headless} \
     run.motion_file=${motion_file} \
     run.initial_pose_file=${initial_pose_file} \
